4d46b9ce7401fbb51972954bb9df042f6be72a6d
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2026-03-10 11:55:39 +0100

    maj code

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,19 +8,35 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexController extends AbstractController
 {
-    // #[Route('/', name: 'app_index')]
-    // public function index(): Response
-    // {
-    //     return $this->render('index/index.html.twig', [
-    //         'controller_name' => 'IndexController',
-    //     ]);
-    // }
-
-    #[Route('/', name: 'app_coming')]
-    public function coming(): Response
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
     {
-        return $this->render('index/coming.html.twig', [
+        return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
     }
+
+    #[Route('/prestation', name: 'app_prestation')]
+    public function prestation(): Response
+    {
+        return $this->render('prestations/prestation.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/activite', name: 'app_activite')]
+    public function activite(): Response
+    {
+        return $this->render('activite/activite.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    // #[Route('/', name: 'app_coming')]
+    // public function coming(): Response
+    // {
+    //     return $this->render('index/coming.html.twig', [
+    //         'controller_name' => 'IndexController',
+    //     ]);
+    // }
 }
